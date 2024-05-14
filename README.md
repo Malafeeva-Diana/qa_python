cf8982abaf6c6b50ade2473908ef58e04e975f23
Diana Malafeeva <malafeevadian@yandex.com>
2024-05-14 11:43:26 +0300

    new_commit

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# qa_python
+# Проектная работа Sprint_4
+
+Для покрытия тестами приложения BooksCollector были использованы методы:
+
+1. add_new_book_ — добавляет новую книгу в словарь без указания жанра. Название книги может содержать максимум 40 символов. Одну и ту же книгу можно добавить только один раз.
+2. set_book_genre  — устанавливает жанр книги, если книга есть в books_genre и её жанр входит в список genre.
+3. get_book_genre_ — выводит жанр книги по её имени.
+4. get_books_with_specific_genre_ — выводит список книг с определённым жанром.
+5. get_books_genre_ — выводит текущий словарь books_genre.
+6. get_books_for_children_ — возвращает книги, которые подходят детям. У жанра книги не должно быть возрастного рейтинга.
+7. add_book_in_favorites_ — добавляет книгу в избранное. Книга должна находиться в словаре books_genre. Повторно добавить книгу в избранное нельзя.
+8. delete_book_from_favorites_ — удаляет книгу из избранного, если она там есть.
+9. get_list_of_favorites_books_ — получает список избранных книг.
+
+
+1. Добавляем две книги
+
+    def test_add_new_book_add_two_books(self):
+
+2. Название книги может содержать максимум 40 символов или без названия
+
+    def test_add_new_book_can_more_40symbols
+
+3. Одну и ту же книгу можно добавить только один раз
+
+    def test_add_new_book_cant_add_two_books
+
+4. Устанавливает жанр книги
+
+    def test_set_book_genre_set_genre
+
+5. Жанр у книги не устанавливается, если ее нет в book_genre
+
+    def test_set_book_genre_cant_get_genre
+
+6. Выводит жанр книги по её имени
+
+    def test_get_book_genre_get_book_name
+
+7. Выводит список книг с определённым жанром
+
+    def test_get_books_with_specific_genre
+
+8. Выводит текущий словарь
+
+    def test_get_books_genre_books_genre
+
+9. Возвращает книги, которые подходят детям
+
+    def test_get_books_for_children
+
+10. Добавляет книгу в избранное
+
+    def test_add_book_in_favorites
+
+11. Повторно добавить книгу в избранное нельзя
+
+    def test_add_book_in_favorites_cant_same_book_in_favorites
+
+12. Удаляет книгу из избранного, если она там есть
+
+    def test_delete_book_from_favorites
+
+13. Получает список избранных книг
+
+    def test_get_list_of_favorites_books
--- tests.py
+++ tests.py
@@ -1,24 +1,96 @@
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
+    # 1. Добавляем две книги -------------------------------------------------------------------------------------------
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(collector.get_books_genre()) == 2
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    # 2. Название книги может содержать максимум 40 символов или без названия ------------------------------------------
+    @pytest.mark.parametrize('name', ['', 'Жизнь, необыкновенные и удивительные приключения Робинзона Крузо'])
+    def test_add_new_book_can_more_40symbols(self, collector, name):
+        collector.add_new_book(name)
+        assert len(collector.get_books_genre()) == 0
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    # 3. Одну и ту же книгу можно добавить только один раз -------------------------------------------------------------
+    def test_add_new_book_cant_add_two_books(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert len(collector.get_books_genre()) == 1
+
+    # 4. Устанавливает жанр книги --------------------------------------------------------------------------------------
+    def test_set_book_genre_set_genre(self, collector):
+        collector.books_genre = {'Гордость и предубеждение и зомби': ''}
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+        assert collector.books_genre.get('Гордость и предубеждение и зомби') == 'Ужасы'
+
+    # 5. Жанр у книги не устанавливается, если ее нет в book_genre -----------------------------------------------------
+    def test_set_book_genre_cant_get_genre(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Роман')
+        assert collector.get_book_genre('Роман') is None
+
+    # 6. Выводит жанр книги по её имени --------------------------------------------------------------------------------
+    def test_get_book_genre_get_book_name(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+        assert collector.get_book_genre('Гордость и предубеждение и зомби') == 'Ужасы'
+
+    # 7. Выводит список книг с определённым жанром ---------------------------------------------------------------------
+    def test_get_books_with_specific_genre(self, collector):
+        collector.books_genre = {'Гарри Поттер и кубок огня': 'Фантастика', 'Трое в лодке': 'Комедии',
+                                 'Дюна': 'Фантастика'}
+        assert len(collector.get_books_with_specific_genre('Фантастика')) == 2
+
+    # 8. Выводит текущий словарь ---------------------------------------------------------------------------------------
+    def test_get_books_genre_books_genre(self, collector):
+        books_genre = {'Гарри Поттер и кубок огня': 'Фантастика', 'Трое в лодке': 'Комедии',
+                       'Что делать, если ваш кот хочет вас убить': 'Детективы',
+                       'Дядя Федор, пес и кот': 'Мультфильмы',
+                       'Гордость и предубеждение и зомби': 'Ужасы'}
+        for name, genre in books_genre.items():
+            collector.add_new_book(name)
+            collector.set_book_genre(name, genre)
+        assert collector.get_books_genre() == books_genre
+
+    # 9. Возвращает книги, которые подходят детям ----------------------------------------------------------------------
+    def test_get_books_for_children(self, collector):
+        books_genre = {'Гордость и предубеждение и зомби': 'Ужасы',
+                       'Что делать, если ваш кот хочет вас убить': 'Детективы',
+                       'Дядя Федор, пес и кот': 'Мультфильмы'}
+        for name, genre in books_genre.items():
+            collector.add_new_book(name)
+            collector.set_book_genre(name, genre)
+        assert collector.get_books_for_children() == ['Дядя Федор, пес и кот']
+
+    # 10. Добавляет книгу в избранное ----------------------------------------------------------------------------------
+    def test_add_book_in_favorites(self, collector):
+        collector.add_new_book('Дядя Федор, пес и кот')
+        collector.add_book_in_favorites('Дядя Федор, пес и кот')
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    # 11. Повторно добавить книгу в избранное нельзя -------------------------------------------------------------------
+    def test_add_book_in_favorites_cant_same_book_in_favorites(self, collector):
+        collector.add_new_book('Дядя Федор, пес и кот')
+        collector.add_book_in_favorites('Дядя Федор, пес и кот')
+        collector.add_book_in_favorites('Дядя Федор, пес и кот')
+        assert len(collector.get_list_of_favorites_books()) != 2
+
+    # 12. Удаляет книгу из избранного, если она там есть ---------------------------------------------------------------
+    def test_delete_book_from_favorites(self, collector):
+        collector.add_new_book('Дядя Федор, пес и кот')
+        collector.add_book_in_favorites('Дядя Федор, пес и кот')
+        collector.delete_book_from_favorites('Дядя Федор, пес и кот')
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    # 13. Получает список избранных книг -------------------------------------------------------------------------------
+    def test_get_list_of_favorites_books(self, collector):
+        collector.add_new_book('Дядя Федор, пес и кот')
+        collector.add_book_in_favorites('Дядя Федор, пес и кот')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert collector.get_list_of_favorites_books() == ['Дядя Федор, пес и кот']
